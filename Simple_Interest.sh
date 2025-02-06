5d2fad4c6265062707ca7c4848b690649d61fd7c
sastryts <165320760+sastryts@users.noreply.github.com>
2025-02-06 11:40:50 -0500

    Update Simple_Interest.sh

--- Simple_Interest.sh
+++ Simple_Interest.sh
@@ -1,1 +1,18 @@
+#This is the file for simple interest calculation
 
+#Script to calculate simple interest
+
+echo "Enter the Principal amount: "
+read principal
+
+echo "Enter the Rate of interest: "
+read roi
+
+echo "Enter the duration: "
+read duration
+
+simple_interest=$(expr $principal \* $roi \* $duration / 100 )
+echo "Simple Interest is: " $simple_interest
+
+amount=$(expr $simple_interest + $principal)
+echo "Meturity Amount is: "$amount
